7567f85b4ad7afbf79622862c3681ba21f169c48
Remi <remi.lesert@gmail.com>
2020-08-12 17:27:00 +0200

    [Code] Ajout du footer et d'une navbar pour le lol

---
+++ scripts/navigation.js
@@ -0,0 +1,6 @@
+$(document).ready(function () 
+{
+    $("#linkedin-link").on('click', function (){window.open("https://www.linkedin.com/in/remi-lesert/");});
+    $("#mail-link").on('click', function (){window.open("mailto:remi.lesert@gmail.com");});
+
+});
