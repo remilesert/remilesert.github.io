e9d6fe562b687c5b5148881ae410e6d2d0b3a08d
Remi <remi.lesert@gmail.com>
2020-08-14 13:40:27 +0200

    [Code] Remplissage des réalisations en cours

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,18 @@
+var fadeTime = 300;
+
 $(document).ready(function () 
 {
     $("#linkedin-link").on('click', function (){window.open("https://www.linkedin.com/in/remi-lesert/");});
     $("#mail-link").on('click', function (){window.open("mailto:remi.lesert@gmail.com");});
 
+    $(".realization").on('mouseenter', ShowOnHover);
+    $(".realization").on('mouseleave', HideOnHover);
+    $(".darkened-pointer").fadeOut(0);
 });
+
+function ShowOnHover() {
+    $(this).children('.darkened-pointer').fadeIn(fadeTime);
+}
+function HideOnHover() {
+    $(this).children('.darkened-pointer').fadeOut(fadeTime);
+}
