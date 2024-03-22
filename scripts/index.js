36c2e3ed4fe365b5a9861f4b7246931482c37d12
Remi Lesert <remi.lesert@gmail.com>
2024-03-22 12:00:37 +0100

    [Code] Début de la révision de la page d'accueil

--- scripts/index.js
+++ scripts/index.js
@@ -9,8 +9,6 @@ $(document).ready(function ()
     $("#linkedin-link").on('click', function (){window.open("https://www.linkedin.com/in/remi-lesert/");});
     $("#mail-link").on('click', function (){window.open("mailto:remi.lesert@gmail.com");});
 
-    $(".realization").on('mouseenter', ShowOnHover);
-    $(".realization").on('mouseleave', HideOnHover);
     $(".realization").on('click', GotoLink);
 
     fetch("/pages/realizationData/frontPage.json").then((response) => {
@@ -22,34 +20,4 @@ $(document).ready(function ()
             });
         })
     });
-
-    
-
-    $(".darkened-pointer").fadeOut(0);
 });
-
-function GotoLink() {
-
-    var index = $(this).attr('id');
-
-    switch (index) {
-        default:
-        case "first":
-            window.location = frontPageJson["first"]["pageName"];
-            break;
-        case "second":
-            window.location = "/pages/JoesStory.html";
-            break;
-        case "third":
-            window.location = "/pages/SecretProject.html";
-            break;
-    }
-}
-
-function ShowOnHover() {
-    $(this).children('.darkened-pointer').fadeIn(fadeTime);
-}
-
-function HideOnHover() {
-    $(this).children('.darkened-pointer').fadeOut(fadeTime);
-}
